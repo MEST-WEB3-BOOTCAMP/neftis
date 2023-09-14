9227c5e1229f861fe87ebf927f23a89378df7355
jkwamlah <jkwamlah@outlook.com>
2023-09-14 10:22:57 +0000

    updated dashboard content

---
+++ packages/nextjs/utils/neftis/contract.ts
@@ -0,0 +1,13 @@
+import { ethers } from 'ethers';
+import {neftisABI} from '~~/generated/neftis';
+
+// Initialize Web3 provider (MetaMask)
+// const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// ABI and contract address of your deployed contract
+const contractAddress = '0x0dFA1DcCF07d8f41b42c9C8da2eCCb98b97aD539';
+
+// Create a contract instance
+// const contract = new ethers.Contract(contractAddress, neftisABI, provider);
+
+// export default contract
