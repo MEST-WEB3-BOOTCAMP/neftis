37b887fb78dd1d1c94d643d797dae6179f87b501
Mike Attara <mpyebattara@gmail.com>
2023-09-14 10:10:33 +0000

    add tests

--- packages/hardhat/test/Nefti.ts
+++ packages/hardhat/test/Nefti.ts
@@ -23,8 +23,27 @@ describe("Nefti", function () {
 
   describe("Minting", function () {
     it("Should mint 1000 token to the deployer", async function () {
-      await nefti.mint(signers[0].address, "url", 1000);
+      await nefti.mint("url", 1000);
       expect(await nefti.balanceOf(signers[0].address, 1)).to.equal(1000);
+    });
+  });
+
+  describe("Transfer", function () {
+    it("Should transfer 100 token to the second account", async function () {
+      await nefti.safeTransferFrom(signers[0].address, signers[1].address, 1, 100, "0x00");
+      expect(await nefti.balanceOf(signers[1].address, 1)).to.equal(100);
+    });
+
+    it("Should fail to transfer 1000 token to the second account", async function () {
+      await expect(nefti.safeTransferFrom(signers[0].address, signers[1].address, 1, 1000, "0x00")).to.be.revertedWith(
+        "ERC1155: insufficient balance for transfer",
+      );
+    });
+
+    it("Should fail to transfer 100 token to the second account if not owner or operator", async function () {
+      await expect(nefti.safeTransferFrom(signers[1].address, signers[2].address, 1, 100, "0x00")).to.be.revertedWith(
+        "ERC1155: caller is not owner or approved operator",
+      );
     });
   });
 });
